2bead5caf0d48669ef05d7d708ffd06c21d2ce40
Gari885 <garinovoselskyyjaka@gmail.com>
2025-12-14 19:15:36 +0100

    Vistas Actualizadas

--- AppGestionVoluntariado/app/src/main/java/com/example/appgestionvoluntariado/Fragments/Ajustes/PerfilDatosVoluntarioFragment.java
+++ AppGestionVoluntariado/app/src/main/java/com/example/appgestionvoluntariado/Fragments/Ajustes/PerfilDatosVoluntarioFragment.java
@@ -7,10 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import com.example.appgestionvoluntariado.Fragments.SesionGlobal;
 import com.example.appgestionvoluntariado.R;
 
 public class PerfilDatosVoluntarioFragment extends Fragment {
+
+    private EditText nombre,email,zona;
+
+    private ImageButton editar;
 
 
     @Override
@@ -22,6 +29,29 @@ public class PerfilDatosVoluntarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_datos_voluntario, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil_datos_voluntario, container, false);
+        nombre = view.findViewById(R.id.etNombre);
+        email = view.findViewById(R.id.etEmail);
+        editar = view.findViewById(R.id.btnActivarEdicion);
+
+
+
+        nombre.setText(SesionGlobal.getNombre());
+        email.setText(SesionGlobal.getEmail());
+
+
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desbloquearCampos();
+            }
+        });
+
+        return view;
+    }
+
+    private void desbloquearCampos() {
+        nombre.setEnabled(true);
+        email.setEnabled(true);
     }
 }
