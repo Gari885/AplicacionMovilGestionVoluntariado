28ebdf6344554a1d87b7503c3cb038ca26db9095
Gari885 <garinovoselskyyjaka@gmail.com>
2025-12-14 21:49:22 +0100

    Vistas añadidas

--- AppGestionVoluntariado/app/src/main/java/com/example/appgestionvoluntariado/Fragments/VistaOrganizador/CrearMatchFragment.java
+++ AppGestionVoluntariado/app/src/main/java/com/example/appgestionvoluntariado/Fragments/VistaOrganizador/CrearMatchFragment.java
@@ -1,4 +1,4 @@
-package com.example.appgestionvoluntariado;
+package com.example.appgestionvoluntariado.Fragments.VistaOrganizador;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.appgestionvoluntariado.R;
 
 
 public class CrearMatchFragment extends Fragment {
